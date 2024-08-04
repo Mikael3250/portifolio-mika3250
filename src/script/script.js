7a774a06c15d99121a47be195f901b16d7b067d3
Mikael Bondan <122181666+Mikael3250@users.noreply.github.com>
2024-08-04 18:19:41 -0300

    Add files via upload

---
+++ src/script/script.js
@@ -0,0 +1,8 @@
+// código do botão do celular
+
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('ul.menu').classList.toggle('show');
+})
+
+//  FIM do código do botão do celular
+
